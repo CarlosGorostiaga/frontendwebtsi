a1eed73d7291609da829b0c29f045250b1b10a16
Carlos Gorostiaga <carlosgorospo@gmail.com>
2025-09-18 13:32:17 +0200

    Subida de components y cambios en layout

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    server: {
+      watch: {
+        usePolling: true,
+        interval: 300, // milisegundos (puedes bajar a 100 si quieres más rápido)
+      },
+      host: true,      // expone en 0.0.0.0 dentro de Docker
+      strictPort: true // evita que cambie de puerto si ya está ocupado
+    }
   }
 });
